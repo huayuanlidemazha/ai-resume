00071aeb5d49e797466f1dafec1a0af559f5b701
李岩 <2977235904@qq.com>
2026-03-06 19:04:10 +0800

    Add2

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: './',
+  base: '/ai-resume/',
+  build: {
+    outDir: 'dist',     // 确认输出目录
+    assetsDir: 'assets' // 确认资源目录
+  },
   plugins: [vue()],
   server: {
     port: 5173,
